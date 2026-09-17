04c77e3e815942133437f40ccf5df0cc5c5c6b65
Jeevalingam <vh15134@velhightech.com>
2026-09-18 00:57:38 +0530

    Add Tower of Hanoi state representation

--- src/hanoi_search.py
+++ src/hanoi_search.py
@@ -8,17 +8,30 @@ def create_goal_state(num_disks):
     return ((), (), tuple(range(num_disks, 0, -1)))
 
 
+def is_goal_state(state, num_disks):
+    """Check whether the current state is the goal state."""
+    return state == create_goal_state(num_disks)
+
+
+def display_state(state):
+    """Display the three pegs."""
+    print("Peg A:", state[0])
+    print("Peg B:", state[1])
+    print("Peg C:", state[2])
+
+
 def main():
     num_disks = 3
 
     initial_state = create_initial_state(num_disks)
-    goal_state = create_goal_state(num_disks)
 
-    print("Tower of Hanoi Search Model")
-    print("---------------------------")
-    print("Number of disks:", num_disks)
-    print("Initial state:", initial_state)
-    print("Goal state:", goal_state)
+    print("Tower of Hanoi State Representation")
+    print("-----------------------------------")
+
+    print("\nInitial State:")
+    display_state(initial_state)
+
+    print("\nIs goal state?", is_goal_state(initial_state, num_disks))
 
 
 if __name__ == "__main__":
